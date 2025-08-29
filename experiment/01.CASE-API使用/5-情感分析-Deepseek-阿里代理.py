ca1b842cbf12769822e4dc0fb9e659a6b559462a
lihzsky <lihaizh_cn@foxmail.com>
2025-08-29 23:00:54 +0800

    feat: 添加 5-情感分析-Deepseek-阿里代理

---
+++ experiment/01.CASE-API使用/5-情感分析-Deepseek-阿里代理.py
@@ -0,0 +1,26 @@
+import os
+import dashscope
+from dotenv import load_dotenv
+from dashscope.api_entities.dashscope_response import Role
+
+load_dotenv(verbose=True)
+dashscope.api_key = os.getenv("DASHSCOPE_API_KEY")
+
+def get_response(messages):
+  response = dashscope.Generation.call(
+    model="deepseek-r1",
+    messages=messages,
+    result_format="message"
+  )
+  
+  return response
+
+# 测试对话
+messages = [
+    {"role": "system", "content": "You are a helpful assistant"},
+    {"role": "user", "content": "你好，你是什么大模型？"}
+]
+
+response = get_response(messages)
+
+print(response.output.choices[0].message.content)
